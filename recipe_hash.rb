311d471ab97847242efeedb1d78a85a3ab7ca4ec
merryshen <yihuaca@gmail.com>
2018-07-12 18:21:24 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,28 +1,14 @@
-#code your solutions here
-
-#1. Create a hash called `perfect_10_recipe` that has the following data:
-#  - almond flour - 2 cups
-#  - gluten freewhole oats - 1 cup
-#  - kosher salt - 1/2 tea spoon
-#  - baking powder  - 1/2 tea spoon
-#  - baking soda - 1/4 tea spoon
-#  - Xanthan Gum - 1/2 tea spoon
-#  - slivered almonds, lightly toasted - 1/4 cup
-#  - mini dark chocolate chips - 3/4 cup
-#  - olive oil - 1/2 cup
-#  - agave - 1/4 cup
-# All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
-
 perfect_10_recipe = {
-  :almond_flour => "2 cups",
-  :gluten_freewhole_oats => "1 cup",
-  :kosher_salt => "1/2 tea spoon",
-  :baking_powder => "1/2 tea spoon",
-  :Xantham_Gum => "1/2 tea spoon",
-  :silvered_almonds, lightly toasted => "1/4 cup",
-  :mini_dark_chocolate_chips => "3/4 cup",
-  :olive_oil => "1/2 cup",
-  :agave => "1/4 cup"
+        :almond_flour => "2 cups",
+        :gluten_free_whole_oats => "1 cup",
+        :kosher_salt => "1/2 tea spoon",
+        :baking_powder => "1/2 tea spoon",
+        :baking_soda => "1/4 tea spoon",
+        :xanthan_Gum => "1/2 tea spoon",
+        :slivered_almonds => "1/4 cup",
+        :mini_dark_chocolate_chips => "3/4 cup",
+        :olive_oil => "1/2 cup",
+        :agave => "1/4 cup"
 }
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
@@ -35,26 +21,28 @@ def amount_of_chocolate_chips(perfect_10_recipe)
  perfect_10_recipe[:mini_dark_chocolate_chips]
 end
 
-#3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
-
+#3: Use puts to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the each method.
 def ingredients_and_amounts(perfect_10_recipe)
-perfect_10_recipe.each do |ingredient, amount|
+ perfect_10_recipe.each do |ingredient, amount|
    puts "#{ingredient} #{amount}"
+ end
 end
 
-#4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
+#4: Now use puts to print out just the ingredients without the amounts. You'll want to use the each_key method.
 def ingredients(perfect_10_recipe)
-  perfect_10_recipe.each_key do |ingredients|
+ perfect_10_recipe.each_key do |ingredients|
    puts "#{ingredients}"
+ end
 end
 
-#5: Now use `puts` to print out just the amounts without the ingredients. You'll want to use the `each_value` method.
+#5: Now use puts to print out just the amounts without the ingredients. You'll want to use the each_value method.
 def amounts(perfect_10_recipe)
-  perfect_10_recipe.each_value do |amounts|
+ perfect_10_recipe.each_value do |amounts|
    puts "#{amounts}"
+ end
 end
 
-#6: Return the number of ingredients in the `perfect_10_recipe` hash.
+#6: Return the number of ingredients in the perfect_10_recipe hash.
 def number_ingredients(perfect_10_recipe)
-  perfect_10_recipe.length
+ perfect_10_recipe.length
 end
